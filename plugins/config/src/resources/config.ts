7d02b076d119b6072f20ce0fa0b740438db762ff
eugene <evgenijantonenkov456@gmail.com>
2026-05-25 01:12:16 +0300

    fix(config): update version to 0.0.6 in package.json; add Resource decorator to ConfigurationResource class

--- plugins/config/src/resources/config.ts
+++ plugins/config/src/resources/config.ts
@@ -1,5 +1,7 @@
+import { Resource } from "@draug/engine";
 import { Configuration } from "../config/config";
 
+@Resource({ name: "ConfigurationResource" })
 export class ConfigurationResource {
     public readonly config = new Configuration();
 }
